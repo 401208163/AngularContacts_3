16de26608dba0b5a216e4a71455621ac1901335d
k <401208163@qq.com>
2019-10-18 17:05:25 +0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component'
 
+import { ContactListComponent } from './contact-list/contact-list.component'
+import { ContactEditComponent} from './contact-edit/contact-edit.component'
+import { ContactNewComponent } from './contact-new/contact-new.component'
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { from } from 'rxjs';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/contacts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contacts',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ContactListComponent
+      }
+    ]
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
