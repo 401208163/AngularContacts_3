10da211441dadf53296361211bca4ef276dde29b
k <401208163@qq.com>
2019-10-20 15:52:33 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
+
+// 开启 HTTP 功能
+// open the root AppModule,
+// import the HttpClientModule symbol from @angular/common/http,
+// add it to the @NgModule.imports array.
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -14,32 +22,32 @@ import { ContactEditComponent } from './contact-edit/contact-edit.component';
 import { TagListComponent } from './tag-list/tag-list.component';
 import { TagNewComponent } from './tag-new/tag-new.component';
 import { TagEditComponent } from './tag-edit/tag-edit.component';
+import { AppRoutingModule } from './/app-routing.module';
 import { LayoutComponent } from './layout/layout.component';
-import { from } from 'rxjs';
+
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavbarComponent,
-      SidebarComponent,
-      SigninComponent,
-      SignupComponent,
-      ContactListComponent,
-      ContactNewComponent,
-      ContactEditComponent,
-      TagListComponent,
-      TagNewComponent,
-      TagEditComponent,
-      LayoutComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      FormsModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SigninComponent,
+    SignupComponent,
+    ContactListComponent,
+    ContactNewComponent,
+    ContactEditComponent,
+    TagListComponent,
+    TagNewComponent,
+    TagEditComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,12 +14,31 @@ export class SignupComponent implements OnInit {
     password: ''
   };
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  email_err_msg = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
 
   signup() {
-    console.log('submit');
+    const formData = this.signupForm;
+    this.http.post('http://localhost:3000/users', formData)
+      .toPromise()
+      .then((data: any) => {
+        this.email_err_msg = '';
+        window.localStorage.setItem('auth_token', data.token);
+        window.localStorage.setItem('user_info', JSON.stringify(data.user));
+        this.router.navigate(['/']);
+      })
+      .catch(err => {
+        if (err.status === 409) {
+          this.email_err_msg = '邮箱已被占用';
+        }
+      });
   }
 }
