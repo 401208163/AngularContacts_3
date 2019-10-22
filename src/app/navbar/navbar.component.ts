3029f75f32e10120cd48bdf83c3602ac9c085575
k <401208163@qq.com>
2019-10-22 12:35:07 +0800

    用户推出

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user = JSON.parse(window.localStorage.getItem('user_info') || '{}');
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+  }
+
+  signout(e) {
+    e.preventDefault();
+    this.http.delete('http://106.15.206.216:3000/session')
+      .toPromise()
+      .then(data => {
+        window.localStorage.removeItem('auth_token');
+        this.router.navigate(['/signin']);
+      })
+      .catch(err => {
+        window.alert('退出失败，请稍后重试');
+      });
   }
 
 }
